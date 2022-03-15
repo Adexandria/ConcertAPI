97c3667da164df3e5293e6bbe454ded32ded76e8
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-15 10:10:07 +0100

    identity user custom model

--- Concert.Domain/Entities/UserModel.cs
+++ Concert.Domain/Entities/UserModel.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Concert.Domain.Entities
 {
     public class UserModel:IdentityUser
     {
+        [Key]
+        public override string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public override string UserName { get; set; }
+        public override string Email { get; set; }
+        public override string PasswordHash { get; set; }
+        public override bool TwoFactorEnabled { get; set; } = false;
     }
 }
