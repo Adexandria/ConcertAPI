ea0c36f9aa2a33c40e59589a0805f1acb2ee04e2
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-15 10:11:17 +0100

    identity context for user model

---
+++ ConcertAPI.Infrastructure/Service/AuthDbService.cs
@@ -0,0 +1,18 @@
+﻿using Concert.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concert.Infrastructure.Service
+{
+    public class AuthDbService :IdentityDbContext<UserModel>
+    {
+        public AuthDbService(DbContextOptions<AuthDbService> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
