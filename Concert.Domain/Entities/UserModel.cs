3f972c3ddb3b5751ae8ad21b53dd46b949b4a90c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-15 08:11:35 +0100

    Add project files.

---
+++ Concert.Domain/Entities/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concert.Domain.Entities
+{
+    public class UserModel:IdentityUser
+    {
+    }
+}
