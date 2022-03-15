53b7f31e7cf86d074ef402f505a9217723aaaee7
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-15 10:14:33 +0100

    configuration for identity server 4 and asp.net core identity,

--- ConcertAPI/Startup.cs
+++ ConcertAPI/Startup.cs
@@ -1,7 +1,14 @@
+using Concert.Domain.Entities;
+using Concert.Infrastructure.Service;
+using ConcertAPI.In_Memory;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +16,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ConcertAPI
@@ -25,12 +33,52 @@ namespace ConcertAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
             services.AddControllers();
+
+            services.AddIdentity<UserModel, IdentityRole>()
+                 .AddEntityFrameworkStores<AuthDbService>()
+                    .AddDefaultTokenProviders();
+            services.AddDbContext<AuthDbService>(s => s.UseSqlServer(Configuration["ConnectionStrings:Concert"],
+                sql=>sql.MigrationsAssembly(migrationsAssembly)));
+            services.AddIdentityServer()
+                .AddTestUsers(IdentityConfiguration.TestUsers)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(Configuration["ConnectionStrings:Concert"],
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(Configuration["ConnectionStrings:Concert"],
+                        sql => sql.MigrationsAssembly(migrationsAssembly));
+                }).AddAspNetIdentity<UserModel>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies")
+                    .AddOpenIdConnect("oidc", options =>
+                    {
+                        options.Authority = "http://localhost:13391";
+
+                        options.ClientId = "mvc";
+                        options.ClientSecret = "secret";
+                        options.ResponseType = "code";
+
+                        options.SaveTokens = true;
+                        options.RequireHttpsMetadata = false;
+                        options.Scope.Add("api1");
+                       
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            InitializeDatabase(app);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,6 +87,7 @@ namespace ConcertAPI
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
@@ -46,6 +95,42 @@ namespace ConcertAPI
             {
                 endpoints.MapControllers();
             });
+        }
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in IdentityConfiguration.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityConfiguration.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in IdentityConfiguration.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
