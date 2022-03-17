1c4851f3051b91f1c3ef3dd3f6da4949f14e8478
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-17 10:44:28 +0100

    created an identity controller

---
+++ ConcertAPI/Controllers/IdentityController.cs
@@ -0,0 +1,25 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConcertAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
