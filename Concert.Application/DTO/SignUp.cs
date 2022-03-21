9febda847c23c9a157e289174ac97dca15e6c418
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 12:15:04 +0100

    user DTO for signing up

---
+++ Concert.Application/DTO/SignUp.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Concert.Application.DTO
+{
+    
+    public class SignUp
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public  string Email { get; set; }
+        [Required]
+        public  string Password { get; set; }
+        [Required]
+        public string RetypePassword { get; set; }
+    }
+}
