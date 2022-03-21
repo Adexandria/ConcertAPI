1509f6895260060a322940bfc7223d636bc3a679
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 20:06:24 +0100

    login model

---
+++ Concert.Application/DTO/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concert.Application.DTO
+{
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
