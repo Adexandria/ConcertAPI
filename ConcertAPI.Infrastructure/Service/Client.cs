f168397b2e62e5c73978aeae9908132416b264ad
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 12:16:34 +0100

    a client to connect to identity server

---
+++ ConcertAPI.Infrastructure/Service/Client.cs
@@ -0,0 +1,41 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Concert.Infrastructure.Service
+{
+    public class Client
+    {
+        public Client(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        private readonly IConfiguration Configuration;
+
+        public async Task<string> RequestTokenAsync()
+        {
+            HttpClient client = new HttpClient();
+            DiscoveryDocumentResponse disco = await client.GetDiscoveryDocumentAsync("http://localhost:34085");
+            if (disco.IsError)
+            {
+                return disco.Error;
+            }
+            
+            TokenResponse tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "myApi.read"
+            });
+            if (tokenResponse.IsError)
+            {
+                return tokenResponse.Error;
+            }
+            return tokenResponse.AccessToken;
+        }
+        
+    }
+}
