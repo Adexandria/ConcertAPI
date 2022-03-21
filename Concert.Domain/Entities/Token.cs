6103c1f030946dfe39e3fb96ffad2ab884182e9c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 12:15:26 +0100

    A token object

---
+++ Concert.Domain/Entities/Token.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concert.Domain.Entities
+{
+    public class Token
+    {
+        public string GeneratedToken { get; set; }
+    }
+}
