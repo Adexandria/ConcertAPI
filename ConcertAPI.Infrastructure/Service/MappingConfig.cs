36e649c060c291874d44dc7997ef8eed524c7e99
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 12:16:46 +0100

    mapster mapping config

---
+++ ConcertAPI.Infrastructure/Service/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using Concert.Application.DTO;
+using Concert.Domain.Entities;
+using Mapster;
+
+
+namespace Concert.Infrastructure.Service
+{
+    public class MappingConfig
+    {
+        public TypeAdapterConfig UserModelConfig()
+        {
+            var config = TypeAdapterConfig<SignUp, UserModel>.NewConfig()
+                .Map(dest => dest.PasswordHash, src => src.Password);
+            return config.Config;
+        }
+        
+    }
+}
