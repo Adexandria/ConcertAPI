00e80e680fb81f375dcc147eb24e4084fa32c5b0
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-21 12:15:57 +0100

    identity services

---
+++ ConcertAPI.Infrastructure/Service/IdentityServices.cs
@@ -0,0 +1,39 @@
+﻿using Concert.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Infrastructure.Service
+{
+    public class IdentityServices
+    {
+        readonly UserManager<UserModel> userManager;
+        public IdentityServices(UserManager<UserModel> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<string> EmailConfirmationToken(UserModel user)
+        {
+            return await userManager.GenerateEmailConfirmationTokenAsync(user);
+
+        }
+        public async Task AddUserRole(UserModel user , string role)
+        {
+            await userManager.AddToRoleAsync(user, role);
+        }
+        public async Task AddUserClaim(UserModel user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+              new Claim(ClaimTypes.Name,user.FirstName),
+              new Claim(ClaimTypes.Role,"User"),
+              new Claim(ClaimTypes.Email,user.Email)
+            };
+            await userManager.AddClaimsAsync(user, claims);
+           
+        }
+    }
+}
