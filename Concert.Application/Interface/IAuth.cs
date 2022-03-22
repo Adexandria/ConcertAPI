43553f19eca1c96e1a0eaa4ee7a8a487cf17f5b9
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 22:04:17 +0100

    authentication interface

---
+++ Concert.Application/Interface/IAuth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+   public interface IAuth
+    {
+        string TwoFactorAuthentication(string email);
+        bool KeyVerification(string email, string key);
+           
+    }
+}
