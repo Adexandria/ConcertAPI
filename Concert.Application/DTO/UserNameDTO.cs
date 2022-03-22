9368493aeae8d476d6157d3947bee555efcf185c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 09:29:33 +0100

    change username model DTO

---
+++ Concert.Application/DTO/UserNameDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concert.Application.DTO
+{
+    public class UserNameDTO
+    {
+        public string UserName { get; set; }
+    }
+}
