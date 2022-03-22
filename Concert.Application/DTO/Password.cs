e4f02776e01560acb0042dc7ecdb41c181de9801
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 09:29:16 +0100

    password model DTO

---
+++ Concert.Application/DTO/Password.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Concert.Application.DTO
+{
+    public class Password
+    {
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string RetypePassword { get; set; }
+        [Required]
+        public string Token { get; set; }
+    }
+}
