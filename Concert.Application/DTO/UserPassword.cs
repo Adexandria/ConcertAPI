65e3240eb45e1174b3b48386795951bc6d36d9c9
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 22:04:00 +0100

    change password model

---
+++ Concert.Application/DTO/UserPassword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Concert.Application.DTO
+{
+   public class UserPassword
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
