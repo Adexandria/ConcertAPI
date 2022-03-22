903d6970f15175cd8f0050fb0432018427350112
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 22:06:36 +0100

    added  two factor authentication and verification

--- ConcertAPI/Controllers/SettingsController.cs
+++ ConcertAPI/Controllers/SettingsController.cs
@@ -1,4 +1,5 @@
 ﻿using Concert.Application.DTO;
+using Concert.Application.Interface;
 using Concert.Domain.Entities;
 using Concert.Infrastructure.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -17,21 +18,20 @@ namespace ConcertAPI.Controllers
     [ApiController]
     public class SettingsController : ControllerBase
     {
-        readonly IPasswordHasher<UserModel> passwordHasher;
         readonly UserManager<UserModel> userManager;
-        readonly EmailService emailService;
-        public SettingsController(UserManager<UserModel> userManager, EmailService emailService, IPasswordHasher<UserModel> passwordHasher)
+        readonly IAuth auth;
+        public SettingsController(UserManager<UserModel> userManager, IAuth auth)
         {
             this.userManager = userManager;
-            this.emailService = emailService;
-            this.passwordHasher = passwordHasher;
+            this.auth = auth;
+            
         }
 
 
     
 
         ///<param name="userName">
-        ///\a user's username
+        /// user's username
         ///</param>
         ///<param name="newUserName">
         ///an object used to change user's username
@@ -41,7 +41,8 @@ namespace ConcertAPI.Controllers
         /// </summary>
         /// 
         /// <returns>A string status</returns>
-        [HttpPost("{username}/username/change")]
+        [HttpPost("{userName}/change")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<ActionResult> ChangeUsername(UserNameDTO newUserName, string userName)
         {
             try
@@ -67,6 +68,111 @@ namespace ConcertAPI.Controllers
                 return BadRequest(e.Message);
             }
 
+        }
+
+        ///<param name="userName">
+        ///user's username
+        ///</param>
+        ///<param name="userPassword">
+        ///an object used to change the authorized user's password
+        ///</param>
+        /// <summary>
+        /// change password
+        /// </summary>
+        /// 
+        /// <returns>A string status</returns>
+        [HttpPost("{userName}/password")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> ChangePassword(string userName,UserPassword userPassword)
+        {
+            try
+            {
+                var currentUser = await userManager.FindByNameAsync(userName);
+                if (currentUser == null)
+                {
+                    return NotFound("username doesn't exist");
+                }
+               
+                var result = await userManager.ChangePasswordAsync(currentUser, userPassword.CurrentPassword, userPassword.NewPassword);
+                if (result.Succeeded)
+                {
+                    return this.StatusCode(StatusCodes.Status200OK, "Successfully Changed");
+                }
+
+                return BadRequest(result.Errors);
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        ///<param name="userName">
+        /// user's username
+        ///</param>
+        /// <summary>
+        /// Get Authentication Key
+        /// </summary>
+        /// 
+        /// <returns>A string status</returns>
+        [HttpGet("{userName}/Authentication")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> TwoFactorAuthentication(string userName)
+        {
+            try
+            {
+                var currentUser = await userManager.FindByNameAsync(userName);
+                if (currentUser == null)
+                {
+                    return NotFound("username doesn't exist");
+                }
+                return Ok(auth.TwoFactorAuthentication(currentUser.Email));
+             
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        ///<param name="userName">
+        /// user's username
+        ///</param>
+        ///<param name="key">
+        ///Google verification Key
+        ///</param>
+        /// <summary>
+        /// Enabled Two authentication
+        /// </summary>
+        /// 
+        /// <returns>A string status</returns>
+        [HttpPost("{userName}/Authentication/{key}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> AuthenticationKeyVerification(string userName, string key)
+        {
+            try
+            {
+                var currentUser = await userManager.FindByNameAsync(userName);
+                if (currentUser == null)
+                {
+                    return NotFound("username doesn't exist");
+                }
+                if (auth.KeyVerification(currentUser.Email, key))
+                {
+                    await userManager.SetTwoFactorEnabledAsync(currentUser,true);
+                    return Ok("Successful");
+                }
+                return BadRequest("Invalid Key, Try Again");
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
         }
     }
 }
