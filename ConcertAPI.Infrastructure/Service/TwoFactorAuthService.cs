4c6829b2be07c5da2608c7299a79af6b1dd5a75e
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-22 22:04:35 +0100

    authentication repository

---
+++ ConcertAPI.Infrastructure/Service/TwoFactorAuthService.cs
@@ -0,0 +1,42 @@
+﻿using Concert.Application.Interface;
+using Google.Authenticator;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Infrastructure.Service
+{
+    public class TwoFactorAuthService : IAuth
+    {
+        
+        readonly IConfiguration _configuration;
+        public TwoFactorAuthService( IConfiguration _configuration)
+        {
+            this._configuration = _configuration;
+        }
+
+        public bool KeyVerification(string email, string key)
+        {
+            var twoFactorSecretCode = _configuration["TwoFactorSecretCode"];
+            string accountSecretKey = $"{twoFactorSecretCode}-{email}";
+            var twoFactorAuthenticator = new TwoFactorAuthenticator();
+            var result = twoFactorAuthenticator
+                .ValidateTwoFactorPIN(accountSecretKey, key);
+            return result;
+        }
+
+        public string TwoFactorAuthentication(string email)
+        {
+            var twoFactorAuthenticator = new TwoFactorAuthenticator();
+            var twoFactorSecretCode = _configuration["TwoFactorSecretCode"];
+            var accountSecretKey = $"{twoFactorSecretCode}-{email}";
+            var setupCode = twoFactorAuthenticator.GenerateSetupCode("Concert API", email,
+                Encoding.ASCII.GetBytes(accountSecretKey));
+            return setupCode.ManualEntryKey;
+        }
+
+        
+    }
+}
