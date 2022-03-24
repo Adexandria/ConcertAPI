90845946cb858610dad7947ce732113fc19173c3
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:04:59 +0100

    added concert sponsor model

---
+++ Concert.Domain/Entities/Concert/ConcertSponsor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class ConcertSponsor
+    {
+        [Key]
+        public Guid ConcertSponsorId { get; set; }
+        [ForeignKey("Sponsor")]
+        public Guid SponsorId { get; set; }
+        [ForeignKey("ConcertModel")]
+        public Guid ConcertId { get; set; }
+        public Sponsor Sponsor { get; set; }
+        public ConcertModel Concert { get; set; }
+    }
+}
