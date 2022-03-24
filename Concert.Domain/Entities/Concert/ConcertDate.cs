19517538fff2e3548ca75b9eab9f4749167f5833
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:05:21 +0100

    added date model

---
+++ Concert.Domain/Entities/Concert/ConcertDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class ConcertDate
+    {
+        [Key]
+        public Guid DateId { get; set; }
+        public DateTime DateTime { get; set; }
+        [ForeignKey("ConcertModel")]
+        public Guid ConcertId { get; set; }
+        public ConcertModel Concert { get; set; }
+    }
+}
