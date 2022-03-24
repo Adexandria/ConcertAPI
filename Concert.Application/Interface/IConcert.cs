e97949dcc4758101595e10f21565e8c93ffbda5d
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:11:43 +0100

    concert interface

---
+++ Concert.Application/Interface/IConcert.cs
@@ -0,0 +1,22 @@
+﻿using Concert.Domain.Entities.Concert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+    public interface IConcert
+    {
+        //Concert Model
+        IEnumerable<ConcertModel> GetConcerts { get; }
+        IEnumerable<ConcertModel> GetConcertsbyName(string name);
+        Task AddConcert(ConcertModel concert);
+        Task<ConcertModel> UpdateConcert(ConcertModel concert);
+        Task DeleteConcert(ConcertModel concert);
+
+
+
+        
+    }
+}
