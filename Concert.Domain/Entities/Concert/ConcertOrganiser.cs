1479341698a3def1a5d87407273def44e9e0d183
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:04:47 +0100

    added concert organiser model

---
+++ Concert.Domain/Entities/Concert/ConcertOrganiser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class ConcertOrganiser
+    {
+        [Key]
+        public Guid ConcertOrganiserId { get; set; }
+        [ForeignKey("Organiser")]
+        public Guid OrganiserId { get; set; }
+        [ForeignKey("ConcertModel")]
+        public Guid ConcertId { get; set; }
+        public Organiser Organiser { get; set; }
+        public ConcertModel Concert { get; set; }
+    }
+}
