0baa1955d098b8272fd1ea84572ec36fc8c0ca88
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:10:49 +0100

    model for artist

---
+++ Concert.Domain/Entities/Concert/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class Artist
+    {
+        [Key]
+        public Guid ArtistId { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
