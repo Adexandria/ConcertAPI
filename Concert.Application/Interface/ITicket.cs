4e67bb42b8fb0bcfce58d781379f4eb7a7c2538f
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:12:44 +0100

    ticket interface

---
+++ Concert.Application/Interface/ITicket.cs
@@ -0,0 +1,16 @@
+﻿using Concert.Domain.Entities.Concert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+    public interface ITicket
+    {
+        IEnumerable<Ticket> SearchTicketByPrice(int price);
+        Task AddTicket(Ticket ticket);
+        Task<Ticket> UpdateTicket(Ticket ticket);
+        Task DeleteTicket(Ticket ticket);
+    }
+}
