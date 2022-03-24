7e5e9aedb5789c256c785e28774d41e4f0d6e8ba
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:04:14 +0100

    added organiser model

---
+++ Concert.Domain/Entities/Concert/Organiser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class Organiser
+    {
+        [Key]
+        public Guid OrganiserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+  
+    }
+}
