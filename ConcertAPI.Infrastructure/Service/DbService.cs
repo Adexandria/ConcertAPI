60ddbfddd1b373202fdac93ea8026fa93a9f19b7
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:03:15 +0100

    dbcontext for concert db service

---
+++ ConcertAPI.Infrastructure/Service/DbService.cs
@@ -0,0 +1,25 @@
+﻿using Concert.Domain.Entities.Concert;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Concert.Infrastructure.Service
+{
+    public class DbService:DbContext
+    {
+        public DbService(DbContextOptions<DbService> options):base(options)
+        {
+
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<ConcertArtist> ConcertArtists { get; set; }
+        public DbSet<Organiser> Organiser { get; set; }
+        public DbSet<ConcertOrganiser> ConcertOrganiser { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<ConcertSponsor> ConcertSponsors { get; set; }
+        public DbSet<ConcertDate> ConcertDate { get; set; }
+        public DbSet<ConcertModel> Concerts { get; set; }
+    }
+}
