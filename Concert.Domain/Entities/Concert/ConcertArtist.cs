7dcad184e10ccbb17799ffcda7e7af69f659d356
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:05:31 +0100

    added concert artist model

---
+++ Concert.Domain/Entities/Concert/ConcertArtist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class ConcertArtist
+    {
+        [Key]
+        public Guid ConcertArtistId { get; set; }
+        [ForeignKey("Artist")]
+        public Guid ArtistId { get; set; }
+        [ForeignKey("ConcertModel")]
+        public Guid ConcertId { get; set; }
+        public Artist Artist { get; set; }
+        public ConcertModel Concert { get; set; }
+    }
+}
