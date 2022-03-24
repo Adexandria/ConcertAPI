daaff88d13957767b81bbf1401abbed69c235f7a
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:11:00 +0100

    Artist interface

---
+++ Concert.Application/Interface/IArtist.cs
@@ -0,0 +1,26 @@
+﻿using Concert.Domain.Entities.Concert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+    public interface IArtist
+    {
+        //Artist
+        Task AddArtist(Artist artist);
+        Task<Artist> UpdateArtist(Artist artist);
+        Task DeleteArtist(Artist artist);
+    
+
+
+
+        //Concert Artist
+        IEnumerable<ConcertArtist> GetConcertArtists { get; }
+        IEnumerable<ConcertArtist> GetArtistByName(string name);
+        Task AddConcertArtist(ConcertArtist artist);
+        Task<ConcertArtist> UpdateConcertArtist(ConcertArtist artists);
+        Task DeleteConcertArtist(ConcertArtist artists);
+    }
+}
