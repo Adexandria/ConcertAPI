d144643ec6f9b89e26bb9eeb7ed9f4109cb923ec
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:05:12 +0100

    added concert model

---
+++ Concert.Domain/Entities/Concert/ConcertModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Concert.Domain.Entities.Concert
+{
+    public class ConcertModel
+    {
+        [Key]
+        public Guid ConcertId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+    }
+}
