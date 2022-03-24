1c14d21bd2b68413b6464efd5a108fe1bff29a63
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:12:31 +0100

    sponsor interface

---
+++ Concert.Application/Interface/ISponsor.cs
@@ -0,0 +1,26 @@
+﻿using Concert.Domain.Entities.Concert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+    public interface ISponsor
+    {
+        //Sponsors
+        Task AddSponsor(Sponsor sponsor);
+        Task<Sponsor> UpdateSponsor(Sponsor sponsor);
+        Task DeleteSponsor(Sponsor sponsor);
+
+
+
+
+        //Concert Sponsors
+        IEnumerable<ConcertSponsor> GetConcertSponsors { get; }
+        IEnumerable<ConcertSponsor> GetSponsorByName(string name);
+        Task AddConcertSponsor(ConcertSponsor sponsor);
+        Task<ConcertSponsor> UpdateConcertSponsor(ConcertSponsor sponsor);
+        Task DeleteConcertSponsor(ConcertSponsor sponsor);
+    }
+}
