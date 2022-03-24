08b4ca58a15824f606e5634b78d9ecad568ef1c4
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:02:40 +0100

    Db migration

---
+++ ConcertAPI/Migrations/20220324095952_userdb.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConcertAPI.Migrations
+{
+    public partial class userdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Concerts",
+                columns: table => new
+                {
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Concerts", x => x.ConcertId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organiser",
+                columns: table => new
+                {
+                    OrganiserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organiser", x => x.OrganiserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sponsors",
+                columns: table => new
+                {
+                    SponsorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SponsorLevel = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sponsors", x => x.SponsorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConcertArtists",
+                columns: table => new
+                {
+                    ConcertArtistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ArtistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConcertArtists", x => x.ConcertArtistId);
+                    table.ForeignKey(
+                        name: "FK_ConcertArtists_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConcertArtists_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConcertDate",
+                columns: table => new
+                {
+                    DateId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConcertDate", x => x.DateId);
+                    table.ForeignKey(
+                        name: "FK_ConcertDate_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Package = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConcertOrganiser",
+                columns: table => new
+                {
+                    ConcertOrganiserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrganiserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConcertOrganiser", x => x.ConcertOrganiserId);
+                    table.ForeignKey(
+                        name: "FK_ConcertOrganiser_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConcertOrganiser_Organiser_OrganiserId",
+                        column: x => x.OrganiserId,
+                        principalTable: "Organiser",
+                        principalColumn: "OrganiserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConcertSponsors",
+                columns: table => new
+                {
+                    ConcertSponsorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SponsorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ConcertId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConcertSponsors", x => x.ConcertSponsorId);
+                    table.ForeignKey(
+                        name: "FK_ConcertSponsors_Concerts_ConcertId",
+                        column: x => x.ConcertId,
+                        principalTable: "Concerts",
+                        principalColumn: "ConcertId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConcertSponsors_Sponsors_SponsorId",
+                        column: x => x.SponsorId,
+                        principalTable: "Sponsors",
+                        principalColumn: "SponsorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertArtists_ArtistId",
+                table: "ConcertArtists",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertArtists_ConcertId",
+                table: "ConcertArtists",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertDate_ConcertId",
+                table: "ConcertDate",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertOrganiser_ConcertId",
+                table: "ConcertOrganiser",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertOrganiser_OrganiserId",
+                table: "ConcertOrganiser",
+                column: "OrganiserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertSponsors_ConcertId",
+                table: "ConcertSponsors",
+                column: "ConcertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConcertSponsors_SponsorId",
+                table: "ConcertSponsors",
+                column: "SponsorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ConcertId",
+                table: "Tickets",
+                column: "ConcertId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConcertArtists");
+
+            migrationBuilder.DropTable(
+                name: "ConcertDate");
+
+            migrationBuilder.DropTable(
+                name: "ConcertOrganiser");
+
+            migrationBuilder.DropTable(
+                name: "ConcertSponsors");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Organiser");
+
+            migrationBuilder.DropTable(
+                name: "Sponsors");
+
+            migrationBuilder.DropTable(
+                name: "Concerts");
+        }
+    }
+}
