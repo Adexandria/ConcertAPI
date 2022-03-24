a2d0ac99d9f99d2a882a80f716430a627902b6df
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-03-24 12:12:12 +0100

    Organiser interface

---
+++ Concert.Application/Interface/IOrganiser.cs
@@ -0,0 +1,25 @@
+﻿using Concert.Domain.Entities.Concert;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Application.Interface
+{
+    public interface IOrganiser
+    {
+        //Organiser
+        Task AddOrganiser(Organiser organiser);
+        Task<Organiser> UpdateOrganiser(Organiser organiser);
+        Task DeleteOrganiser(Organiser organiser);
+
+
+        //Concert Organiser
+        IEnumerable<ConcertOrganiser> GetConcertOrganisers { get; }
+        IEnumerable<ConcertOrganiser> GetOrganiserByName(string name);
+        IEnumerable<ConcertOrganiser> GetOrganiserByConcert(string name);
+        Task AddConcertOrganiser(ConcertOrganiser organiser);
+        Task<ConcertOrganiser> UpdateConcertOrganiser(ConcertOrganiser organiser);
+        Task DeleteConcertOrganiser(ConcertOrganiser organiser);
+    }
+}
